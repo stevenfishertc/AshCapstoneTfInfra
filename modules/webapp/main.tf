189b0d9bc19cc21aecd526f16dee194c6eba0a64
Steven Fisher <steven.fisher@techconsulting.tech>
2025-12-23 08:49:27 -0500

    free service plan

--- modules/webapp/main.tf
+++ modules/webapp/main.tf
@@ -3,8 +3,8 @@ resource "azurerm_service_plan" "plan" {
   location            = var.location
   resource_group_name = var.resource_group_name
 
-  os_type  = "Linux"
-  sku_name = "D1"
+  os_type  = "Windows"
+  sku_name = "F1"
 
   tags = var.tags
 }
