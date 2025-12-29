26f593b3f36c99c6e8a37b4a31e5a4400654d46a
Steven Fisher <steven.fisher@techconsulting.tech>
2025-12-29 11:14:22 -0500

    fixes3

--- modules/acr_private_access/outputs.tf
+++ modules/acr_private_access/outputs.tf
@@ -1,5 +1,1 @@
-output "acr_id" {
-  value = data.azurerm_container_registry.acr.id
-}
-
 output "private_dns_zone_acr_id" { value = azurerm_private_dns_zone.acr.id }
--- modules/aks/outputs.tf
+++ modules/aks/outputs.tf
@@ -1,3 +1,3 @@
 output "id"   { value = azurerm_kubernetes_cluster.aks.id }
 output "name" { value = azurerm_kubernetes_cluster.aks.name }
-output "kubelet_identity" { value = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id }
+output "kubelet_id" { value = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id }
