2d5e656eac2b58c685196f7ca5f185740ff73716
Steven Fisher <steven.fisher@techconsulting.tech>
2026-01-04 09:50:30 -0500

    identity index

--- modules/webapp/main.tf
+++ modules/webapp/main.tf
@@ -44,5 +44,5 @@ resource "azurerm_linux_web_app" "webapp" {
 resource "azurerm_role_assignment" "webapp_acr_pull" {
   scope                = var.acr_id
   role_definition_name = "AcrPull"
-  principal_id         = azurerm_linux_web_app.webapp.identity.principal_id
+  principal_id         = azurerm_linux_web_app.webapp.identity[0].principal_id
 }
