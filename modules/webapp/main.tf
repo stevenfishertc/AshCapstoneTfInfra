e1491262ffb38b5a29c781da61692350ef643fb9
Steven Fisher <steven.fisher@techconsulting.tech>
2026-01-03 19:12:29 -0500

    vnet route all

--- modules/webapp/main.tf
+++ modules/webapp/main.tf
@@ -31,6 +31,7 @@ resource "azurerm_linux_web_app" "webapp" {
     "WEBSITE_RUN_FROM_PACKAGE" = "1"
     "WEBSITE_DNS_SERVER"       = "168.63.129.16"
     "WEBSITE_DNS_ALT_SERVER"   = "8.8.8.8"
+    "WEBSITE_VNET_ROUTE_ALL"   = "1"
     "REACT_APP_API_URL"        = ""   # Will be set later via pipeline (APIM URL)
     "WEBSITES_PORT"            = "80"
   }
